87e43950c39d85989fac219a5ac28a3fbff16f19
Xinyi0220 <122421708+Xinyi0220@users.noreply.github.com>
2025-03-08 17:52:03 -0700

    Add files via upload

---
+++ ipfs.py
@@ -0,0 +1,23 @@
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+
+PROJECT_ID = "9d9fdc2c7c504c7fb3fae437588bc99e"
+PROJECT_SECRET = "LmpsrZtmimEnzlhS0U9HTWOUnTAUwpKkzoUBgnBkeo70zEYxmcBwVg"
+
+def pin_to_ipfs(data):
+	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
+	#YOUR CODE HERE
+	url = "https://ipfs.infura.io:5001/api/v0/add"
+	json_data = json.dumps(data)
+	files = {'file': ('data.json', json_data, 'application/json')}
+	response = requests.post(url, files=files, auth=HTTPBasicAuth(INFURA_PROJECT_ID, INFURA_PROJECT_SECRET))
+
+	return cid
+
+def get_from_ipfs(cid,content_type="json"):
+	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
+	#YOUR CODE HERE
+	
+	assert isinstance(data,dict), f"get_from_ipfs should return a dict"
+	return data
