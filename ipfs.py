3066e5bb6cbb2f44a3ffcc520f540e892c00d190
Xinyi0220 <122421708+Xinyi0220@users.noreply.github.com>
2025-03-08 17:57:56 -0700

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -12,8 +12,11 @@ def pin_to_ipfs(data):
 	json_data = json.dumps(data)
 	files = {'file': ('data.json', json_data, 'application/json')}
 	response = requests.post(url, files=files, auth=HTTPBasicAuth(PROJECT_ID, PROJECT_SECRET))
-
-	return cid
+	if response.status_code == 200:
+    		cid = response.json().get("Hash")  # Extract CID from response
+    		return cid
+  	else:
+    		raise Exception(f"IPFS upload failed: {response.text}")
 
 def get_from_ipfs(cid,content_type="json"):
 	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
